bcae2ddcda9999296194e36e2bcbf56a148f76f8
Dominic Torruellas <dom@domtorr.com>
2019-05-08 19:19:38 -0700

    basic first pass of HeaderComponent done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <div>Lambda School</div>
+      <div>@LambdaSchool - 06 May</div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
