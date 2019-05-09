cb91d4f272ee5209a610ab06c72591492db6cd4d
Dominic Torruellas <dom@domtorr.com>
2019-05-08 19:39:34 -0700

    basic first pass of CardComponent

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+  return (
+    <div>
+      <div>Get started with React</div>
+      <div>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application.
+      </div>
+      <div>reactjs.org</div>
+    </div>
+  );
+};
+
+export default CardContent;
