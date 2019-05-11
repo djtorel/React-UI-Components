e1b5c71950d6b78f4bc0a7662cd020d11a225ff3
Dominic Torruellas <dom@domtorr.com>
2019-05-11 11:28:21 -0700

    finished styling using css grid

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -9,32 +9,26 @@ const App = () => {
   return (
     <div className="calculator-container">
       <CalculatorDisplay />
-      <div className="button-container">
-        <ActionButton text="clear" />
-        <NumberButton buttonStyle="red-button" text="÷" />
-      </div>
-      <div className="button-container">
-        <NumberButton buttonStyle="white-button" text="7" />
-        <NumberButton buttonStyle="white-button" text="8" />
-        <NumberButton buttonStyle="white-button" text="9" />
-        <NumberButton buttonStyle="red-button" text="×" />
-      </div>
-      <div className="button-container">
-        <NumberButton buttonStyle="white-button" text="4" />
-        <NumberButton buttonStyle="white-button" text="5" />
-        <NumberButton buttonStyle="white-button" text="6" />
-        <NumberButton buttonStyle="red-button" text="−" />
-      </div>
-      <div className="button-container">
-        <NumberButton buttonStyle="white-button" text="1" />
-        <NumberButton buttonStyle="white-button" text="2" />
-        <NumberButton buttonStyle="white-button" text="3" />
-        <NumberButton buttonStyle="red-button" text="+" />
-      </div>
-      <div className="button-container">
-        <ActionButton text="0" />
-        <NumberButton buttonStyle="red-button" text="=" />
-      </div>
+      <ActionButton className="span-3" text="clear" />
+      <NumberButton className="span-1" buttonStyle="red-button" text="÷" />
+
+      <NumberButton className="span-1" buttonStyle="white-button" text="7" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="8" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="9" />
+      <NumberButton className="span-1" buttonStyle="red-button" text="×" />
+
+      <NumberButton className="span-1" buttonStyle="white-button" text="4" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="5" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="6" />
+      <NumberButton className="span-1" buttonStyle="red-button" text="−" />
+
+      <NumberButton className="span-1" buttonStyle="white-button" text="1" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="2" />
+      <NumberButton className="span-1" buttonStyle="white-button" text="3" />
+      <NumberButton className="span-1" buttonStyle="red-button" text="+" />
+
+      <ActionButton className="span-3" text="0" />
+      <NumberButton className="span-1" buttonStyle="red-button" text="=" />
     </div>
   );
 };
